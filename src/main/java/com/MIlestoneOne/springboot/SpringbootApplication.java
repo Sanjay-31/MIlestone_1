8c0c275daf5b19e1fef05c32f6d6c511fc67c1dd
Sanjay-31 <shakya.sanjay.31@gmail.com>
2022-02-14 02:39:36 +0530

    Updated

--- src/main/java/com/MIlestoneOne/springboot/SpringbootApplication.java
+++ src/main/java/com/MIlestoneOne/springboot/SpringbootApplication.java
@@ -1,8 +1,5 @@
 package com.MIlestoneOne.springboot;
 
-import com.MIlestoneOne.springboot.model.User;
-import com.MIlestoneOne.springboot.repository.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -13,9 +10,7 @@ public class SpringbootApplication implements CommandLineRunner {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootApplication.class, args);
 	}
-//
-//	@Autowired
-//	private UserRepository userRepository;
+
 
 	@Override
 	public void run(String... args) throws Exception {
